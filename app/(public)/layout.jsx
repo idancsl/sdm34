6b49093250b810dfa58a50d2fc77ccf92b99886b
idancsl <idancsl28@gmail.com>
2026-02-06 10:00:13 +0700

    updatess

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -1,13 +1,16 @@
 "use client";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import { SessionProvider } from "next-auth/react";
 
 export default function PublicLayout({ children }) {
   return (
     <>
+     <SessionProvider>
         <Navbar />
         {children}
         <Footer />
+      </SessionProvider>
       </>
   );
 }
